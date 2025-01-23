e3a4b4b2c77bec6717c4dbf3e4542ce991c3623f
Gareth Ng <wu.qingzhe@outlook.com>
2025-01-23 13:38:51 +0800

    homework5 finished

--- Assignment5/Renderer.cpp
+++ Assignment5/Renderer.cpp
@@ -229,8 +229,10 @@ void Renderer::Render(const Scene& scene)
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
-
+            x = (2 * (i + 0.5f) / (float)(scene.width-1) - 1) * imageAspectRatio * scale;
+            y = (1 - 2 * (j + 0.5f) / (float)(scene.height-1)) * scale; // Screen coordinate system origin is at top-left corner
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            dir = normalize(dir);
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
@@ -241,9 +243,9 @@ void Renderer::Render(const Scene& scene)
     (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
     for (auto i = 0; i < scene.height * scene.width; ++i) {
         static unsigned char color[3];
-        color[0] = (char)(255 * clamp(0, 1, framebuffer[i].x));
-        color[1] = (char)(255 * clamp(0, 1, framebuffer[i].y));
-        color[2] = (char)(255 * clamp(0, 1, framebuffer[i].z));
+        color[0] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].x));
+        color[1] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].y));
+        color[2] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].z));
         fwrite(color, 1, 3, fp);
     }
     fclose(fp);    
--- Assignment5/Triangle.hpp
+++ Assignment5/Triangle.hpp
@@ -11,6 +11,23 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+    Vector3f e1 = v1 - v0;
+    Vector3f e2 = v2 - v0;
+    Vector3f s = orig - v0;
+    Vector3f s1 = crossProduct(dir, e2);
+    Vector3f s2 = crossProduct(s, e1);
+
+    float t = dotProduct(s2, e2) / dotProduct(s1, e1);
+    float b1 = dotProduct(s1, s) / dotProduct(s1, e1);
+    float b2 = dotProduct(s2, dir) / dotProduct(s1, e1);
+    float b3 = 1 - b1 - b2;
+
+    if (t > 0 && b1 > 0 && b2 > 0 && b3 > 0) {
+        tnear = t;
+        u = b1;
+        v = b2;
+        return true;
+    }
     return false;
 }
 
